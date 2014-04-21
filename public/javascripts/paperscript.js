b0405023a12804a41232aaa2947bafbfbbf935f8
Kaitlyn Higa <kaitlyn.higa@gmail.com>
2014-04-21 12:34:44 -0700

    updated intersections and select

--- public/javascripts/paperscript.js
+++ public/javascripts/paperscript.js
@@ -1,4 +1,21 @@
+var cData = function(){
+  this.shapeArray = [];
+  this.owner = '';
+  this.created = new Date(year, month, day, hours, minutes, seconds, milliseconds);
+}
+
 var placeTriangleVertices = function(tool, event, vertices, path){
+
+  if(event.shiftKey){
+    console.log("shift key");
+    tool.onMouseUp = function(event) {
+      path = new Path.Circle({
+        center: event.middlePoint,
+        radius: event.delta.length / 2
+      });
+    obj.fillColor = 'black';
+    }
+  }
 
   tool.onMouseDown = function(event){
     console.log("vertices: " + vertices);
@@ -12,19 +29,78 @@ var placeTriangleVertices = function(tool, event, vertices, path){
       tnew.drawTriangle();
     } 
   }
+
 };
 
-var placeCircle = function(tool, event, obj){
 
+var regularTriangle = function(tool, event, path){
   tool.onMouseUp = function(event) {
-    obj = new Path.Circle({
+    path = new Path.Circle({
     center: event.middlePoint,
     radius: event.delta.length / 2
   });
     obj.fillColor = 'black';
   }
-
 }
+
+var placeCircle = function(tool, event, path){
+  if(event.shiftKey){
+    console.log("shift key");
+   }
+  tool.onMouseUp = function(event) {
+    path = new Path.Circle({
+    center: event.middlePoint,
+    radius: event.delta.length / 2
+   });
+    path.fillColor = 'black';
+  }
+
+ }
+
+var select = function(tool, event, options){
+  var path;
+  var items = [];
+  var movePath = false;
+  tool.onMouseDown = function(event, options){
+    path = null;
+    var hitResult = project.hitTest(event.point, options);
+    if(!hitResult){
+      return;
+    }
+    
+    if(hitResult){
+      path = hitResult.item;
+
+      if (event.modifiers.shift) {
+        var lastItem = items.pop();
+        var intersections = path.getIntersections(lastItem);
+
+        for (var i = 0; i < intersections.length; i++) {
+          new Path.Circle({
+            center: intersections[i].point,
+            radius: 5,
+            fillColor: '#009dec'
+          }).removeOnDown();
+        }
+
+        items.push(lastItem);
+
+      } 
+      items.push(path);
+    }
+    return items;
+    
+    movePath = hitResult.type == 'fill';
+    if (movePath){
+      project.activeLayer.addChild(hitResult.item);
+     }
+  }
+  tool.onMouseDrag = function(event) {
+  
+    path.position += event.delta;
+  
+  }
+ }
 
 
 
@@ -37,6 +113,7 @@ var Triangle = function(){
 };
 
 Triangle.prototype.drawTriangle = function(){
+
   var vertices = [];
   placeTriangleVertices(tool, event, vertices, this.path);
   this.path.closed = true;
@@ -56,7 +133,23 @@ Circle.prototype.drawCircle = function(){
 };
 
 
+var Selection = function(){
+  this.items = [];
+  this.cursor = new paper.Tool();
+  this.cursor.activate();
+}
 
+Selection.prototype.getSelection = function(){
+  var hitOptions = {
+    segments: true,
+    stroke: true,
+    fill: true,
+    tolerance: 5
+  };
+
+ this.items = select(this.cursor, event, hitOptions, this.items);
+
+}
 
 
 $("#triangle").click(function(){
@@ -72,5 +165,9 @@ $("#circle").click(function(){
   c.drawCircle();
 });
   
-
+$("#select").click(function(){
+  console.log("selection tool");
+  var s = new Selection();
+  s.getSelection();
+});
 
